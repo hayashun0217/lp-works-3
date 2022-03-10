97b26ae3f545284601d6f6572ee6525913557987
早瀬　駿 <hayase0217@gmail.com>
2022-03-11 07:20:13 +0900

    gulpを導入しました。

---
+++ gulpfile.js
@@ -0,0 +1,95 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssSorter = require("css-declaration-sorter");
+const mmq = require("gulp-merge-media-queries");
+
+const browserSync = require("browser-sync");
+
+//ファイル圧縮
+const cleanCss = require("gulp-clean-css");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+
+const htmlBeautify = require("gulp-html-beautify");
+
+function test(done) {
+  console.log("hello Gulp");  //処理の内容
+  done();
+}
+
+function compileSass() {
+  //元になるフォルダー src
+  return gulp.src("./src/assets/sass/**/*.scss")
+  //コンパイルの処理を記載する
+  .pipe(sass())
+  .pipe(postcss([autoprefixer(), cssSorter()])) 
+  .pipe(mmq())
+  //出力先のフォルダー public
+  .pipe(gulp.dest("./public/assets/css/"))//出力先のパス
+  //CSSファイル圧縮
+  .pipe(cleanCss())
+  .pipe(rename( {
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest("./public/assets/css/"))
+}
+
+
+//scssファイルを更新されたらコンパイル->リロードされる
+function watch() {
+  gulp.watch("./src/assets/sass/**/*.scss", gulp.series(compileSass, browserReload));  
+  gulp.watch("./src/assets/sass/**/*.js", gulp.series(minJs, browserReload));  
+  gulp.watch("./src/assets/img/**/*", gulp.series(copyImage, browserReload));  
+  gulp.watch("./src/assets/**/*.html", gulp.series(formatHTML, browserReload));  
+}
+
+function browserInit() {
+  browserSync.init({
+    server: {
+      baseDir: "./public/"  //ベースのディレクトリー
+    }
+  })
+}
+
+
+function browserReload(done) {
+  browserSync.reload();
+  done();
+}
+
+//jsファイル圧縮
+function minJs() {
+  return gulp.src("./src/assets//js/**/*.js")
+  .pipe(gulp.dest("./public/assets/js/"))
+  .pipe(uglify())
+  .pipe(rename({
+    suffix: ".min"
+  }))
+  .pipe(gulp.dest("./public/assets/js/"))
+}
+
+//HTMLフォーマット
+function formatHTML() {
+  return gulp.src("./src/**/*.html")
+  .pipe(htmlBeautify({
+    indent_size: 2,
+    indent_with_tabs: true,
+  }))
+  .pipe(gulp.dest("./public/"))
+}
+
+function copyImage() {
+  return gulp.src("./src/assets/img/**/*")
+  .pipe(gulp.dest("./public/assets/img/"))
+}
+
+exports.test = test;    //コマンドで実行  コマンドでnpx gulp test
+exports.compileSass = compileSass;    //コマンドで実行  コマンドでnpx gulp compilesSass
+exports.watch = watch;    //コマンドで実行  コマンドでnpx gulp watch
+exports.browserInit = browserInit;    //コマンドで実行  コマンドでnpx gulp browserInit
+exports.dev = gulp.parallel(browserInit, watch)//カッコ内が自動で行われる
+exports.minJS = minJs;
+exports.formatHTML = formatHTML;
+exports.build = gulp.parallel(formatHTML,minJs,compileSass,copyImage);
---
+++ src/assets/js/script.js
@@ -0,0 +1,94 @@
+$(function() {
+  //drawer-js
+  $(document).ready(function() {
+      $('.drawer').drawer()
+  })
+
+  $(".drawer-hamburger").click(function() {
+    // cssで背景色を切り替え
+  });
+
+  jQuery('a[href^="#"]').click(function() {
+    // 移動速度を指定（ミリ秒）
+    let speed = 300;
+    // hrefで指定されたidを取得
+    let id = jQuery(this).attr("href");
+    // idの値が#のみだったらターゲットをhtmlタグにしてトップへ戻るようにする
+    let target = jQuery("#" == id ? "html" : id);
+    // ページのトップを基準にターゲットの位置を取得
+    let position = jQuery(target).offset().top;
+    // ターゲットの位置までspeedの速度で移動
+    jQuery("html, body").animate(
+      {
+        scrollTop: position - $( '#js-header' ).outerHeight()
+    },
+      speed
+    );
+    return false;
+  });
+
+  
+  jQuery('.q-a-item__question').on('click',function() {
+    jQuery(this).next().slideToggle();
+    jQuery(this).children('.drawer-icon').toggleClass('is-open');
+  });
+  
+  
+
+  let mySwiper = new Swiper('.swiper', {
+    // 以下にオプションを設定
+    loop: true, //最後に達したら先頭に戻る
+    
+    //ページネーション表示の設定
+    pagination: {
+      el: '.swiper-pagination', //ページネーションの要素
+      type: 'bullets', //ページネーションの種類
+      clickable: true, //クリックに反応させる
+    },
+    breakpoints: {
+      // 768px以上の場合
+      300: {
+        slidesPerView: 1.5,
+        spaceBetween: 20,
+      },
+
+      1100: {
+        slidesPerView: 2.7,
+        spaceBetween: 55,
+      }
+    }
+
+
+
+  });
+
+  //contact-form
+  let $form = $( '#js-form' )  //ここは独自に指定
+  $form.submit(function(e) { 
+    $.ajax({ 
+      url: $form.attr('action'), 
+      data: $form.serialize(), 
+      type: "POST", 
+      dataType: "xml", 
+      statusCode: { 
+        0: function() { 
+          //送信に成功したときの処理 
+          //ここは独自に指定
+          $form.slideUp()                 
+          $('#js-success').slideDown()
+        }, 
+        200: function() { 
+          //送信に失敗したときの処理 
+          //ここは独自に指定
+          $form.slideUp()
+          $('#js-error').slideDown()
+        }
+      } 
+    });
+    return false; 
+
+  });
+  
+
+
+});
